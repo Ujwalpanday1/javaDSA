9d1f4283a55037077cdffb33ef65baef4c11a639
ujwalpanday1 <ujwalpanday747@gmail.com>
2024-07-16 13:36:17 +0545

    simplier solution also added

--- longestPrefix.java
+++ longestPrefix.java
@@ -1,3 +1,9 @@
+
+
+//my bruteforce approch 
+
+import java.util.Arrays;
+
 class Solution {
     public String longestCommonPrefix(String[] strs) {
 
@@ -35,12 +41,48 @@ class Solution {
     }
 }
 
+//most optimal way 
+
+//sorting the array to that the first and last string have the significant diff 
+// like if i have the sorted array then the string with lot of common prefix character are nearer to it and the element which is far has least common prefix character
+ class SimpleSolution{
+    public String longestCommonPrefix(String[] strs) {
+
+        if (strs == null || strs.length == 0) {
+           return "";
+       }
+
+       Arrays.sort(strs);
+       int i=0;
+       int length=strs.length;
+       StringBuilder sb=new StringBuilder();
+       while(i<strs[0].length()&&i<strs[length-1].length()){
+        if(strs[0].charAt(i)==strs[length-1].charAt(i)){
+         sb.append(strs[0].charAt(i))   ;
+         i++;
+
+        }
+      
+        else{
+            return sb.toString();
+        }
+
+       }
+    
+       return sb.toString();
+       
+   }
+}
+
+
 public class longestPrefix {
+    
 public static void main(String[] args){
 
     Solution sol=new Solution();
+    SimpleSolution sSol=new SimpleSolution();
     String[] strs={"ujwal","ujwrerer","ujrres"};
     System.out.println(  sol.longestCommonPrefix(strs));
-
+    System.out.println(  sSol.longestCommonPrefix(strs));
 }
 }
